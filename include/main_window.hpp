4bf62c0969f5152c10b617c2dd16d37b17c19e52
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-12-07 22:34:57 -0600

    Added simple code based on docs samples
       - Trying to figure out how to add WebKitView to Gtk app
       - Currently, Home button closes app

---
+++ include/main_window.hpp
@@ -0,0 +1,17 @@
+#ifndef _MAIN_WINDOW_
+#define _MAIN_WINDOW_
+
+#include <gtkmm.h>
+#include <webkit/webkit.h>
+#include <iostream>
+
+namespace
+{
+Gtk::Window* mainDialog = nullptr;
+Glib::RefPtr<Gtk::Application> app;
+
+void on_button_clicked();
+void on_app_activate();
+}
+
+#endif
